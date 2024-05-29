99bb6135f5ef0a278d93c97b8948be1f24dfe1fd
Kelvinkhalis <kelvinchiroodza1914@gmail.com>
2024-05-29 13:39:42 -0700

    0x00-ES6_basic

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+export function taskFirst() {
+	const task = 'I prefer when i can.'; 
+	return task;
+}
+
+export function getLast() {
+	return is 'is okay';
+}
+
+export function taskNext() {
+	let combination = 'but sometimes let';
+	combination += getLast();
+
+	return combination;
+}
---
+++ 0x00-ES6_basic/0-main.js
@@ -0,0 +1,3 @@
+import { taskFirst, taskNext } from './0-constnts.js';
+
+console.log(`${taskFist()}`);
