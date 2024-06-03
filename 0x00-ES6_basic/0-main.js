44b4117bb2e402cb0f8ceb7e3b7aae66dd999565
Kelvinkhalis <kelvinchiroodza1914@gmail.com>
2024-06-03 02:23:02 -0700

    0x00-ES6_basic

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,3 +1,4 @@
-import { taskFirst, taskNext } from './0-constnts.js';
+import { taskFirst, taskNext } from './0-constants.js';
 
-console.log(`${taskFist()}`);
+console.log(`${taskFirst()} ${taskNext()}`);
+
